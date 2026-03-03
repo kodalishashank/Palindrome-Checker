1cab3307fc4a778dc5a03586b999a01655d5eb25
kodalishashank <sk6084@srmist.edu.in>
2026-03-03 13:11:04 +0530

    UC9: Recursive Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scn = new Scanner(System.in);
 
         System.out.println("================================================");
         System.out.println("Welcome to the Palindrome Checker App");
-        System.out.println("Version: 8.0");
+        System.out.println("Version: 9.0");
         System.out.println("================================================");
 
         System.out.print("Enter string to check Palindrome: ");
         String input = scn.nextLine();
 
-        Node head = null;
-        Node tail = null;
-
-        for (int i = 0; i < input.length(); i++) {
-            Node newNode = new Node(input.charAt(i));
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        if (head == null || head.next == null) {
-            System.out.println(input + " is palindrome");
-            scn.close();
-            return;
-        }
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        boolean isPalindrome = true;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
+        boolean isPalindrome = checkPalindrome(input, 0, input.length() - 1);
 
         if (isPalindrome) {
             System.out.println(input + " is palindrome");
@@ -74,17 +25,13 @@ public class Main {
         scn.close();
     }
 
-    private static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
+    private static boolean checkPalindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
 
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
+        if (str.charAt(start) != str.charAt(end))
+            return false;
 
-        return prev;
+        return checkPalindrome(str, start + 1, end - 1);
     }
 }
